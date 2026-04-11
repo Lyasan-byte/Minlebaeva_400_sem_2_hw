e660feae05292ef36220a8a6804c9565e0a6faa7
lajsan <lajsanminlebaeva@mail.ru>
2026-04-10 20:15:03 -0700

    Generating changeLog for semester work db from 04.04.26

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("java")
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.liquibase.gradle") version "2.2.2"
 }
 
 group = "com.lays"
@@ -43,8 +47,27 @@ dependencies {
 
 //    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.3")
+    implementation("org.liquibase:liquibase-core:4.33.0")
+    liquibaseRuntime("org.liquibase:liquibase-core:4.33.0")
+    liquibaseRuntime("org.postgresql:postgresql:$postgresVersion")
+    liquibaseRuntime("info.picocli:picocli:4.6.3")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val props = Properties()
+props.load(file("src/main/resources/db/liquibase.properties").inputStream())
+liquibase {
+    activities.register("main") {
+        arguments = mapOf(
+            "changeLogFile" to props.get("change-log-file").toString() + ".xml",
+            "url" to props.get("url"),
+            "username" to props.get("username"),
+            "password" to props.get("password"),
+            "driver" to  props.get("driver-class-name"),
+        )
+    }
+    runList = "main"
 }
